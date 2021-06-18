30cb4c3a726a5e9095b23ca24f7d32b961a02f21
Shavonne Holton <shavonne.holton@gmail.com>
2021-06-17 23:57:18 -0700

    updated local database

--- apps.js
+++ apps.js
@@ -1,6 +1,18 @@
 const Express = require("express");
 const app = Express();
+const dbConnection = require("./db");
 
-app.listen(3000, () => {
-    console.log(`[Server]: App is listening on 3000.`);
-});
+const controllers = require("./controllers");
+
+app.use("/log", controllers.logController); 
+
+dbConnection.authenticate()
+    .then(() => dbConnection.sync())
+    .then(() => {
+        app.listen(3000, () => {
+            console.log(`[Server]: App is listening on 3000.`);
+        });
+    })
+    .catch((err) => {
+        console.log(`[Server]: Server crashed. Error = ${err}`);
+    });
