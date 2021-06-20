fb714ce11a59b0d1da757470a191d9cc897b02d9
Shavonne Holton <shavonne.holton@gmail.com>
2021-06-19 18:57:31 -0700

    updated responses to the data

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # WorkoutLog 
 
-Added test "user1234" to the database
+## Added test "user1234" to the database
 
 <img src = ./assets/firstEntry.PNG>
+
+## Updated code to create dynamic user data
+
+<img src = ./assets/dynamicData.PNG>
+
+## Dynamic user data with response
+
+<img src = ./assets/dataWithResponse.PNG>
+
+## Return the response as JSON data
+
+<img src = ./assets/jsonResponse.PNG>
--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,12 +1,30 @@
-const router = require("express").Router();
+const router = require('express').Router();
 const { UserModel } = require("../models");
+const { UniqueConstraintError } = require("sequelize/lib/errors");
 
 router.post("/register", async (req, res) => {
+    let { username, passwordhash } = req.body.user;
+    try {
+    const User = await UserModel.create({
+        username, 
+        passwordhash
+    });
 
-    UserModel.create({
-        username: "user1234",
-        passwordhash: "password5678"
-    })
-})
+    res.status(201).json({
+        message: "User successfully registered",
+        user: User
+    });
+    } catch (err) {
+        if (err instanceof UniqueConstraintError) {
+            res.status(409).json({
+                message: "Email already in use",
+            });
+        } else {
+        res.status(500).json({
+            message: "Failed to register user",
+        });
+        }
+    }
+});
 
 module.exports = router;
